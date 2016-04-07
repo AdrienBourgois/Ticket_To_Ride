43a67cdeefce01fcde73a1bb9fd30d7c9b196ad8
Kleine Alexis <k.alexis@student.isartdigital.com>
2016-04-07 16:54:52 +0200

    Space instancies wagon

--- Script/space.gd
+++ Script/space.gd
@@ -1,13 +1,17 @@
-
 extends Spatial
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
+var wagon_res
+var is_occupied
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	wagon_res = preload("res://Scene/wagon.scn")
+	is_occupied = false
 	pass
 
-
+func _on_StaticBody_input_event( camera, event, click_pos, click_normal, shape_idx ):
+	if event.type == InputEvent.MOUSE_BUTTON and Input.is_mouse_button_pressed(BUTTON_LEFT) and is_occupied == false:
+		var wagon = wagon_res.instance()
+		add_child(wagon)
+		print("lol")
+		is_occupied = true
+	pass # replace with function body
