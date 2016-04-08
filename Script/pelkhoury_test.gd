37e50d8974e2b02d11bde4c4d64e04bde216540f
Paul El Khoury <p.el@student.isartdigital.com>
2016-04-08 09:47:30 +0200

    litle dictionary of cities in board.scn

--- Script/pelkhoury_test.gd
+++ Script/pelkhoury_test.gd
@@ -1,12 +1,5 @@
 extends Spatial
-
-# member variables here, example:
-# var a=2
-# var b="textvar"
-var city_scn = preload("res://Scene/city.scn")
+var board_scn = preload("res://Scene/board.scn")
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	add_child(city_scn.instance())
-	pass
+	add_child(board_scn.instance())
