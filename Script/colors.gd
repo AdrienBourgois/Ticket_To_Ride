643ad022f23919fef11c296d7920f468317d4c40
Adrien Bourgois <a.bourgois@student.isartdigital.com>
2016-04-11 14:27:14 +0200

    Add colors constants - Cards can now be diplayed - Each card is unique and has proper parameters

---
+++ Script/colors.gd
@@ -0,0 +1,13 @@
+
+extends Node
+
+const red    = Color(1.0, 0.0, 0.0, 1.0)
+const green  = Color(0.0, 1.0, 0.0, 1.0)
+const blue   = Color(0.0, 0.0, 1.0, 1.0)
+const yellow = Color(1.0, 1.0, 0.0, 1.0)
+const pink   = Color(1.0, 0.0, 0.5, 1.0)
+const orange = Color(1.0, 0.5, 0.0, 1.0)
+const white  = Color(1.0, 1.0, 1.0, 1.0)
+const grey   = Color(0.5, 0.5, 0.5, 1.0)
+const black  = Color(0.0, 0.0, 0.0, 1.0)
+const cyan   = Color(0.235, 0.784, 0.812)
