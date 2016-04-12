cfcdb6ad337375b466d25b5fe98dc74c2c565870
Kleine Alexis <k.alexis@student.isartdigital.com>
2016-04-12 10:37:56 +0200

    Create a number of path

--- Script/board.gd
+++ Script/board.gd
@@ -1,8 +1,10 @@
 
 extends Spatial
 var CityScn = preload("res://Scene/city.scn")
+var PathScn = preload("res://Scene/path.scn")
 
 var Cities = []
+var Paths = []
 var Id = 1
 
 func _ready():
@@ -21,6 +23,8 @@ func create_world():
 	add_city({ "name": "Vilnius", "pos": Vector3(-8, 0, 8) })
 	add_city({ "name": "Saint-Petersbourg", "pos": Vector3(-15, 0, 15) })
 	add_city({ "name": "Rostov", "pos": Vector3(-15, 0, -5) })
+	_add_path({ "pos": Vector3(20, 0, 2), "NbrSpace": 6, "Type": "normal", "color": colors.green})
+	_add_path({ "pos": Vector3(12, 0, 12), "NbrSpace": 4, "Type": "normal", "color": colors.red})
 
 func add_city(CityType):
 	var City = str2var(var2str(CityType))
@@ -31,3 +35,13 @@ func add_city(CityType):
 	add_child(New_city)
 	New_city.translate(City["pos"])
 	New_city.get_node("City").Name = (City["name"])
+
+func _add_path(PathType):
+	var path = str2var(var2str(PathType))
+	path["id"] = Id
+	Paths.append(path)
+	Id += 1
+	var New_path = PathScn.instance()
+	New_path._add_space(path["NbrSpace"], path["color"])
+	add_child(New_path)
+	New_path.translate(path["pos"])
--- Script/path.gd
+++ Script/path.gd
@@ -1,16 +1,35 @@
 extends Spatial
 
-var path = { "NbrSpace": "4", "Type": "normal", "color": Color(0.0,1.0,0.0,1.0)}#colors.green
-
 var space_scene = preload("res://Scene/space.scn")
+var spaces = []
+var id = 1
+var NbrSpace = {}
+var color = {}
 
 func _ready():
-	_set_type_path()
-	var space = space_scene.instance()
-	add_child(space)
-	space.parameter = path["color"]
-	space._set_color_space()
+	#_set_type_path()
 	pass
 
-func _set_type_path():
-	pass
+func _add_space(_NbrSpace, _color):
+	NbrSpace = _NbrSpace
+	color = _color
+	for i in range(NbrSpace):
+		var space = space_scene.instance()
+		space._prepare_space(color,i)
+		add_child(space)
+		spaces.append(space)
+
+ """func _set_type_path():
+	if path["Type"] == "normal":
+		print("Road normal")
+	if path["Type"] == "tunnel":
+		print("Tunnel")
+		print("Pioche 3 Cards")
+	if path["Type"] == "simple ferry":
+		print("Simple ferry")
+	if path["Type"] == "double ferry":
+		print("Double ferry")"""
+
+func signal_clicked():
+	for space in spaces:
+		space._place_wagon()
