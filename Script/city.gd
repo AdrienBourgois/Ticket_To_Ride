585149fc9cbe20a310d34b68c05e078164131eec
Kleine Alexis <k.alexis@student.isartdigital.com>
2016-04-19 14:29:24 +0200

    Adding InfoGame in Gui in game

--- Script/city.gd
+++ Script/city.gd
@@ -22,6 +22,9 @@ func place_a_station(color):
 
 
 func _on_City_input_event( camera, event, click_pos, click_normal, shape_idx ):
+	if click_pos >= get_node("Quad").get_global_transform().origin or click_pos <= get_node("Quad").get_global_transform().origin:
+		print("yes")
+
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		get_node("/root/Game/Player").action("city", self)
 
