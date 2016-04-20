62f221a410262e49a66d22ef794985eb7131647a
Paul El Khoury <p.el@student.isartdigital.com>
2016-04-20 11:48:48 +0200

    autoload numberPlayer, selected number player on solo mode

--- Script/Solo.gd
+++ Script/Solo.gd
@@ -3,7 +3,8 @@ extends TextureButton
 
 func _on_SoloButton_pressed():
 	if (get_parent().get_node("LineEdit").get_text() == "Enter your name") || (get_parent().get_node("LineEdit").get_text() == ""):
-		player_information.player_name = "Player 1"
+		player_information.player_name = "Unamed"
 	else:
 		player_information.player_name = get_parent().get_node("LineEdit").get_text()
+	numberPlayer.number_player = get_parent().get_node("OptionButton").get_selected_ID()
 	get_tree().change_scene("res://Scene/General/game.scn")
---
+++ Script/number_player.gd
@@ -0,0 +1,6 @@
+
+extends Node
+
+var number_player
+
+
