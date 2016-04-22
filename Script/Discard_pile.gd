68bee1cae1477ec1350570eff0761da9090da099
Adrien Bourgois <a.bourgois@student.isartdigital.com>
2016-04-22 18:35:32 +0200

    Hide and show hands - other players hands

--- Script/Discard_pile.gd
+++ Script/Discard_pile.gd
@@ -6,13 +6,16 @@ var cards = []
 func add_card(card):
 	cards.append(card)
 	add_child(card)
+	print(cards)
 
 func get_cards():
 	var cards_to_return = str2var(var2str(cards))
+	var id = 0
 	
 	for card in cards:
 		remove_child(card)
-		cards.remove(card)
+		cards.remove(id)
+		id += 1
 	
 	return cards_to_return
 
